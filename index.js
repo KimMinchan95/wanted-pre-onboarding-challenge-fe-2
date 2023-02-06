40dd0e503b15105f56145e62e43f788d8f1249da
KimMinchan95 <hialscks@gmail.com>
2023-02-06 23:33:44 +0900

    Update Return

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  * @property {boolean} complete - 완료여부
  * @property {string} category - 카테고리
  * @property {string[]} [tags] - 태그들
+ * @returns {void}
  */
 
 /**
@@ -15,6 +16,7 @@
  * @param {string} content - TODO에 추가할 내용
  * @param {string} [category] - 새로 추가하는 할 일의 카테고리
  * @param {string[]} [tags] - 새로 추가하는 할 일의 태그 목록
+ * @return {void}
  */
 const Create = (content, category, tags) => {}
 
@@ -23,7 +25,7 @@ const Create = (content, category, tags) => {}
  * 2. ID를 기반으로 특정 할 일을 조회할 수 있다.
  * @function
  * @param {number} [id] - 있으면 특정 할 일 조회, 없으면 전제 조회
- * @returns {Todo[]} - Todo 목록을 반환한다.
+ * @return {Todo[]} - Todo 목록을 반환한다.
  */
 const Read = (id) => {}
 
@@ -32,6 +34,7 @@ const Read = (id) => {}
  * 2. 특정 할일의 특정 태그를 수정할 수 있다.
  * @function
  * @param {Todo} todo - 수정할 할일
+ * @return {void}
  */
 const Update = (todo) => {}
 
@@ -43,6 +46,6 @@ const Update = (todo) => {}
  * @function
  * @param {string} [id] - 있으면 특정 할 일 삭제, 없으면 전체 삭제
  * @param {string[]} [tags] - 삭제하고 싶은 태그 목록
- * @returns {Todo[]} - Todo 목록을 반환한다.
+ * @return {Todo[]} - Todo 목록을 반환한다.
  */
 const Delete = () => {}
